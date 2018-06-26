9f125bd51bd2c81d5ca7425e42f528f186adb98c
Andre Monello <atmonello@gmail.com>
2018-06-25 22:45:21 -0300

    Created computed properties

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,22 +2,25 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/img/green-socks.jpeg',
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         link: 'https://www.google.com.br/search?q=socks&oq=socks&aqs=chrome..69i57j69i60j0l4.816j0j7&sourceid=chrome&ie=UTF-8',
-        inventory: 100,
-        onSale: false,
         sizes: ['P', 'M', 'G', 'XG'],
         cart: 0,
         variants: [
             {
                 variantId: 01,
                 variantColor: 'green',
-                variantImage: './assets/img/green-socks.jpeg'
+                variantImage: './assets/img/green-socks.jpeg',
+                variantQuantity: 10,
+                variantOnSale: true
             },
             {
                 variantId: 02,
                 variantColor: 'blue',
-                variantImage: './assets/img/blue-socks.jpeg'
+                variantImage: './assets/img/blue-socks.jpeg',
+                variantQuantity: 0,
+                variantOnSale: false
             }
         ],
     },
@@ -28,8 +31,23 @@ var app = new Vue({
         removeFromCart() {
             this.cart--;
         },
-        updateProduct(variantImage){
-            this.image = variantImage;
+        updateProduct(index){
+            this.selectedVariant = index;
         }
-    }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        onSale() {
+            if (this.variants[this.selectedVariant].variantOnSale) return this.title + ' is on Sale!';
+            else return this.title + ' not on Sale! :(';
+        }
+    },
 });
